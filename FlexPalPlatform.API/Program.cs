2658059f4050d192ecd4693ceac64a352834dd6d
RubenGenaro10 <rubengenaro10@gmail.com>
2024-05-29 16:29:09 -0500

    feat(Program.cs): add base configuration

--- FlexPalPlatform.API/Program.cs
+++ FlexPalPlatform.API/Program.cs
@@ -1,16 +1,69 @@
+
+using FlexPalPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add Configuration for Routing
+
+builder.Services.AddControllers( options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 // Add Database Connection
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// Configure Database Context and Logging Levels
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (connectionString == null) return;
+    if (builder.Environment.IsDevelopment()) 
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    else if (builder.Environment.IsProduction()) 
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Error)
+            .EnableDetailedErrors();
+}); 
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title   = "Flex-Pal-Platform.API",
+            Version = "v1",
+            Description = "FlexPal API",
+            TermsOfService = new Uri("https://flexpal.com/tos"),
+            Contact = new OpenApiContact{ Name = "ACME Learning Center", Email = "contact@acme.com" },
+            License = new OpenApiLicense { Name = "Apache 2.0", Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")},
+        });
+    });
+
+// Configure Dependency Injection
+
+// Shared Bounded Context Injection Configuration
 
 var app = builder.Build();
+
+// Verify Database Objects are Created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
