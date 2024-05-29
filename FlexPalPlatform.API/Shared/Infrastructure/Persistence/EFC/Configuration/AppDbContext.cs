0db0fb88782353a2fd5a185ac38dc800a7de4f83
RubenGenaro10 <rubengenaro10@gmail.com>
2024-05-29 16:14:55 -0500

    feat: implement bounded context shared

---
+++ FlexPalPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        base.OnConfiguring(builder);
+        // Enable Audit Fields Interceptors
+        builder.AddCreatedUpdatedInterceptor();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Publishing Context
+        
+       
+        // Apply SnakeCase Naming Convention
+       
+    }
+}
