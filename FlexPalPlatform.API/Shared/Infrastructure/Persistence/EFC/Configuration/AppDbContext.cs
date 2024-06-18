f866dfbf1a6da70f6de723a5bf2d704fc88152fc
LuiggiP <u202218996@upc.edu.pe>
2024-06-18 18:35:53 -0500

    refactor(appdbcontext): Passwor to PasswordHash in Users Contexts

--- FlexPalPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ FlexPalPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,17 +1,17 @@
-﻿
-using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using FlexPalPlatform.API.iam.Domain.Model.Aggregates;
 using FlexPalPlatform.API.Profiles.Domain.Model.Aggregates;
 using FlexPalPlatform.API.Counseling.Domain.Model.Aggregates;
 using FlexPalPlatform.API.Counseling.Domain.Model.Entities;
 using FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
-using FlexPalPlatform.API.Subscriptions.Domain.Model.Aggregates;
 using Microsoft.EntityFrameworkCore;
 
 namespace FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
-public class AppDbContext(DbContextOptions options) : DbContext(options)
+public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         base.OnConfiguring(builder);
@@ -20,53 +20,45 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
+{
+    base.OnModelCreating(builder);
+
+    // Users Context
+    builder.Entity<User>().ToTable("Users");
+    builder.Entity<User>().HasKey(u => u.Id);
+    builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+    builder.Entity<User>().Property(u => u.Username).IsRequired();
+    builder.Entity<User>().Property(u => u.PasswordHash).IsRequired();
+    builder.Entity<User>().Property(u => u.Role).IsRequired();
+
+    // Profiles Context
+    builder.Entity<Profile>().HasKey(p => p.Id);
+    builder.Entity<Profile>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+    builder.Entity<Profile>().OwnsOne(p => p.Name, n =>
     {
-        base.OnModelCreating(builder);
-        
-        // Publishing Context
-        
-        //Users Context
-        builder.Entity<User>().ToTable("Users");
-        builder.Entity<User>().HasKey(u=>u.Id);
-        builder.Entity<User>().Property(u=>u.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<User>().Property(u=>u.Username).IsRequired();
-        builder.Entity<User>().Property(u=>u.PasswordHash).IsRequired();
-        builder.Entity<User>().Property(u=>u.Role).IsRequired();
+        n.WithOwner().HasForeignKey("Id");
+        n.Property(p => p.FirstName).HasColumnName("FirstName");
+        n.Property(p => p.LastName).HasColumnName("LastName");
+    });
+    builder.Entity<Profile>().OwnsOne(p => p.Email, e =>
+    {
+        e.WithOwner().HasForeignKey("Id");
+        e.Property(a => a.Address).HasColumnName("EmailAddress");
+    });
+    builder.Entity<Profile>().OwnsOne(p => p.Phone, n =>
+    {
+        n.WithOwner().HasForeignKey("Id");
+        n.Property(p => p.Number).HasColumnName("PhoneNumber");
+    });
+    builder.Entity<Profile>().OwnsOne(p => p.Role, n =>
+    {
+        n.WithOwner().HasForeignKey("Id");
+        n.Property(p => p.Role).HasColumnName("RoleType");
+    });
+    builder.Entity<Profile>().Property(p => p.Weight);
+    builder.Entity<Profile>().Property(p => p.Height);
 
-        
-        //Profiles Context
-        builder.Entity<Profile>().HasKey(p=>p.Id);
-        builder.Entity<Profile>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Profile>().OwnsOne(p => p.Name, n =>
-        {
-            n.WithOwner().HasForeignKey("Id");
-            n.Property(p => p.FirstName).HasColumnName("FirstName");
-            n.Property(p => p.LastName).HasColumnName("LastName");
-        });
-        
-        builder.Entity<Profile>().OwnsOne(p => p.Email,
-            e =>
-            {
-                e.WithOwner().HasForeignKey("Id");
-                e.Property(a => a.Address).HasColumnName("EmailAddress");
-            });
-
-        builder.Entity<Profile>().OwnsOne(p => p.Phone,
-            n =>
-            {
-                n.WithOwner().HasForeignKey("Id");
-                n.Property(p=>p.Number).HasColumnName("PhoneNumber");
-            });
-        builder.Entity<Profile>().OwnsOne(p => p.Role,
-            n =>
-            {
-                n.WithOwner().HasForeignKey("Id");
-                n.Property(p=>p.Role).HasColumnName("RoleType");
-            });
-        builder.Entity<Profile>().Property(p => p.Weight);
-        builder.Entity<Profile>().Property(p => p.Height);
-        
-         // FitnessPlans Context
+    // FitnessPlans Context
         builder.Entity<FitnessPlan>().ToTable("FitnessPlans");
         builder.Entity<FitnessPlan>(entity =>
         {
@@ -119,28 +111,10 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
             .WithMany(fp => fp.NutritionalMeals)
             .HasForeignKey(nm => nm.FitnessPlanId);
     });
-    
-    // Subscription Context
-        
-    builder.Entity<Subscription>().HasKey(p=>p.Id);
-    builder.Entity<Subscription>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
-    builder.Entity<Subscription>().OwnsOne(p => p.StartDate, n =>
-    {
-        n.WithOwner().HasForeignKey("Id");
-        n.Property(p => p.Value).HasColumnName("StartDate");
-    });
-    builder.Entity<Subscription>().OwnsOne(p => p.EndDate, n =>
-    {
-        n.WithOwner().HasForeignKey("Id");
-        n.Property(p => p.Value).HasColumnName("EndDate");
-    });
-    builder.Entity<Subscription>().OwnsOne(p => p.Status, n =>
-    {
-        n.WithOwner().HasForeignKey("Id");
-        n.Property(p => p.IsActive).HasColumnName("IsActive");
-        n.Property(p => p.Type).HasColumnName("Type");
-    });
-        // Apply SnakeCase Naming Convention
-       builder.UseSnakeCaseWithPluralizedTableNamingConvention();   
-    }
+
+    // Apply SnakeCase Naming Convention
+    builder.UseSnakeCaseWithPluralizedTableNamingConvention();
+}
+
+
 }
